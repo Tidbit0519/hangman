31275e226c4bebbf62d88d71099428ed2ae0a984
Jason Ban Tze Tan <jtbt1218@gmail.com>
2022-08-09 14:27:31 -1000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const randomWords = require('random-word')
 const inquirer = require('inquirer')
 
-//
+/**
+ * Hangman
+ * A program that plays Hangman.
+ * @author Jason Ban Tze Tan
+ * @version 1.0
+ */
+
+/*The function to get user input
+*/
 async function getAnswer() {
     const answer = await inquirer
         .prompt([
@@ -19,6 +27,10 @@ async function getAnswer() {
     return answer.key;
 }
 
+/*The function to split the randomly generated word and return it as an array from by each letter.
+@returns [letters]
+*/
+
 async function splitLetters(word) {
     let letters = [];
     for(let i=0; i<word.length; i++) {
@@ -26,6 +38,10 @@ async function splitLetters(word) {
     };
     return letters
 }
+
+/*The function to that draws hangman depending on the number of attempts user has left.
+@param counter
+*/
 
 function drawHangman(counter) {
         if(counter === 7) {
@@ -87,30 +103,34 @@ function drawHangman(counter) {
     }
 }
 
-
+/*The main function that controls the flow of the program.
+*/
 async function main () {
     console.clear();
     console.log("Welcome to Hangman!");
     const word = randomWords()
+    //For developing reference. Feel free to remove this line of code to play the game for real.
     console.log('This is just for developing reference. The word is ' + word)
     const letters = await splitLetters(word);
+    //For developing reference. Feel free to remove this line of code to play the game for real.
     console.log(letters)
     console.log("Please guess the word. (One letter at a time). There are " + word.length + " letters.");
-
+    
+    
     let condition = true
     let attempts = 7;
     let guessed = []
     while(attempts>0 && condition){
         const answer = await getAnswer();
+        //Ends the game if user guessed all the letters correctly.
         if(guessed.length===word.length) {
-            //     console.log(guessed)
             console.log('You won!')
             break;
         }
+        //Display the result of correctly guessed words.
         else if (word.match(answer)) {
             console.clear();
             drawHangman(attempts);
-            // guessed.push(answer);
             console.log('Correct!')
             for(let i=0; i<word.length; i++) {
                 if(answer === word[i]) {
@@ -120,6 +140,7 @@ async function main () {
                     continue;
                 }
             }
+        //Shows the amount of user attempts left.
         } else {
             console.clear();
             console.log(guessed);
@@ -127,6 +148,7 @@ async function main () {
             drawHangman(attempts);
             attempts = attempts-1;
             console.log('You have ' + attempts + ' attempts left.' )
+            //Ends the game if user used up all attempts.
             if(attempts === 0) {
                 console.log('You have lost the game.')
             }
